2b7d96784aa40ea263d6cb9cbafd646b143e28f3
arrianne <arrianneoshea@hotmail.co.uk>
2020-02-03 14:40:26 +1100

    adding privacy message to notify users to allow camera. Added debugging tools so the device plots their starting position relative to the real world and also adds feature points

--- World Tracker/ViewController.swift
+++ World Tracker/ViewController.swift
@@ -8,17 +8,26 @@
 
 import UIKit
 import ARKit
+
 class ViewController: UIViewController {
     @IBOutlet weak var sceneView: ARSCNView!
+    
     
     //this is used to track the positio and orientation of device relative to the real world
     //
     let configuration = ARWorldTrackingConfiguration()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuration)
+        // Do any additional setup after loading the view, typically from a nib.
     }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
 
 
 }
