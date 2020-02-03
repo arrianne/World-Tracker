0c38318c503020f8efe14acbf6c376afe0977c63
arrianne <arrianneoshea@hotmail.co.uk>
2020-02-03 14:01:33 +1100

    setting up ar world tracking to track position and orientation of device relative to the real world

--- World Tracker/ViewController.swift
+++ World Tracker/ViewController.swift
@@ -1,18 +1,23 @@
 //
 //  ViewController.swift
-//  World Tracker
+//  World Trakcer
 //
 //  Created by Arrianne O'shea on 03/02/2020.
 //  Copyright © 2020 Arrianne Barker. All rights reserved.
 //
 
 import UIKit
-
+import ARKit
 class ViewController: UIViewController {
-
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    //this is used to track the positio and orientation of device relative to the real world
+    //
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.sceneView.session.run(configuration)
     }
 
 
